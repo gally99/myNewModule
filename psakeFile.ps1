07356269ee68b9e3918bd3596c3abe523db85191
Joey Piccola <joey.piccola@ad.piccola.us>
2020-05-04 19:23:24 -0600

    add UploadTestResults

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -3,8 +3,16 @@ properties {
     # This modifies the default behavior from the "Build" task
     # in the PowerShellBuild shared psake task module
     $PSBPreference.Build.CompileModule = $false
+
+    $testResultsPath = Join-Path -Path $PSScriptRoot -ChildPath "output/$env:BHProjectName/testResults.xml"
+    $PSBPreference.Test.OutputFile = $testResultsPath
 }
 
 task default -depends Test
 
 task Test -FromModule PowerShellBuild -Version '0.4.0'
+
+task UploadTestResults {
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $testResultsPath)
+} -description 'Uploading tests'
